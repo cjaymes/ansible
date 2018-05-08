63634b9819c4d05f4b6a355fcaa13fa4f343518f
Casey Jaymes <cjaymes@users.noreply.github.com>
2018-05-08 19:19:26 -0400

    added not_contains

--- test_plugins/tests.py
+++ test_plugins/tests.py
@@ -6,8 +6,16 @@ def contains(value, content):
     except Exception, e:
         raise errors.AnsibleFilterError('contains plugin error: %s, string=%s' % str(e),str(string) )
 
+def not_contains(value, content):
+    try:
+        return content not in value
+    except Exception, e:
+        raise errors.AnsibleFilterError('contains plugin error: %s, string=%s' % str(e),str(string) )
+
+
 class TestModule(object):
     def tests(self):
         return {
             'contains': contains,
+            'not_contains': not_contains,
         }
