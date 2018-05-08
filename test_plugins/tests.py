6279b38e39a4fe20de5872a2f55795d1a539840d
Casey Jaymes <cjaymes@users.noreply.github.com>
2018-05-08 19:33:40 -0400

    contains -> containing

--- test_plugins/tests.py
+++ test_plugins/tests.py
@@ -1,21 +1,21 @@
 from ansible import errors
 
-def contains(value, content):
+def containing(value, content):
     try:
         return content in value
     except Exception, e:
-        raise errors.AnsibleFilterError('contains plugin error: %s, string=%s' % str(e),str(string) )
+        raise errors.AnsibleFilterError('containing plugin error: %s, string=%s' % str(e),str(string) )
 
-def not_contains(value, content):
+def not_containing(value, content):
     try:
         return content not in value
     except Exception, e:
-        raise errors.AnsibleFilterError('contains plugin error: %s, string=%s' % str(e),str(string) )
+        raise errors.AnsibleFilterError('not_containing plugin error: %s, string=%s' % str(e),str(string) )
 
 
 class TestModule(object):
     def tests(self):
         return {
-            'contains': contains,
-            'not_contains': not_contains,
+            'containing': containing,
+            'not_containing': not_containing,
         }
