2c0b2217201fc053e6d678e07e03a04f98808258
Casey Jaymes <cjaymes@gmail.com>
2018-05-18 21:11:09 -0400

    gpgcheck; cronjob note

--- roles/rhel7_repo/files/rhel7_repo_sync.sh
+++ roles/rhel7_repo/files/rhel7_repo_sync.sh
@@ -269,6 +269,6 @@ DISABLED_REPOS=(
 mkdir -p "$SHARE"
 cd "$SHARE"
 for d in ${REPOS[*]}; do
-    reposync --plugins --delete --downloadcomps --download-metadata --repoid=$d
+    reposync --plugins --gpgcheck --delete --downloadcomps --download-metadata --repoid=$d
     createrepo --update --groupfile comps.xml $d
 done
