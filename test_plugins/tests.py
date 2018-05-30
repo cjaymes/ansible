119f7e54f0130aac60ff5571286c47d69dd2060d
Casey Jaymes <cjaymes@gmail.com>
2018-05-30 12:43:26 -0400

    renamed containing > in

--- test_plugins/tests.py
+++ test_plugins/tests.py
@@ -4,22 +4,53 @@ __metaclass__ = type
 
 from ansible import errors
 
-def containing(value, content):
+def _in(value, content):
     try:
         return content in value
     except Exception, e:
-        raise errors.AnsibleFilterError('containing plugin error: %s, string=%s' % str(e),str(string) )
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
 
-def not_containing(value, content):
+def not_in(value, content):
     try:
         return content not in value
     except Exception, e:
-        raise errors.AnsibleFilterError('not_containing plugin error: %s, string=%s' % str(e),str(string) )
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def eq(value, content):
+    try:
+        return value == content
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def even(value):
+    try:
+        return value % 2 == 0
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def odd(value):
+    try:
+        return value % 2 == 1
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def lt(value, content):
+    try:
+        return value < content
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
 
 
 class TestModule(object):
     def tests(self):
         return {
-            'containing': containing,
-            'not_containing': not_containing,
+            'in': _in,
+            'not_in': not_in,
+
+            'eq': eq,
+            'equalto': eq,
+            'even': even,
+            'odd': odd,
+            'lessthan': lt,
+            'lt': lt,
         }
