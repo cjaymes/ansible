230c1a895b0045541c2f7444063d9cd11d752eac
Casey Jaymes <cjaymes@gmail.com>
2018-05-30 13:04:25 -0400

    added tests

--- test_plugins/tests.py
+++ test_plugins/tests.py
@@ -4,18 +4,6 @@ __metaclass__ = type
 
 from ansible import errors
 
-def _in(value, content):
-    try:
-        return content in value
-    except Exception, e:
-        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
-
-def not_in(value, content):
-    try:
-        return content not in value
-    except Exception, e:
-        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
-
 def eq(value, content):
     try:
         return value == content
@@ -28,9 +16,33 @@ def even(value):
     except Exception, e:
         raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
 
-def odd(value):
+def ge(value):
     try:
-        return value % 2 == 1
+        return value >= content
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def gt(value):
+    try:
+        return value > content
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def _in(value, content):
+    try:
+        return content in value
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def not_in(value, content):
+    try:
+        return content not in value
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def le(value, content):
+    try:
+        return value <= content
     except Exception, e:
         raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
 
@@ -40,17 +52,51 @@ def lt(value, content):
     except Exception, e:
         raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
 
+def ne(value, content):
+    try:
+        return value != content
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def none(value):
+    try:
+        return value is None
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
+def odd(value):
+    try:
+        return value % 2 == 1
+    except Exception, e:
+        raise errors.AnsibleFilterError('Test error: %s, string=%s' % str(e),str(string) )
+
 
 class TestModule(object):
     def tests(self):
         return {
+            'eq': eq,
+            'equalto': eq,
+
+            'even': even,
+            'ge': ge,
+            'greaterthanequalto': ge,
+
+            'gt': gt,
+            'greaterthan': gt,
+
             'in': _in,
             'not_in': not_in,
 
-            'eq': eq,
-            'equalto': eq,
-            'even': even,
-            'odd': odd,
-            'lessthan': lt,
+            'le': le,
+            'lessthanequalto': le,
+
             'lt': lt,
+            'lessthan': lt,
+
+            'ne': ne,
+            'notequalto': ne,
+
+            'none': none,
+
+            'odd': odd,
         }
