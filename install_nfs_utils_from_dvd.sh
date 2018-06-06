fb91b075a19c94ad06f8404098f3bce68b1ef5d9
Casey Jaymes <cjaymes@gmail.com>
2018-06-06 11:05:05 -0400

    added dvd nfs-utils install script & cleanup in client_repo

---
+++ install_nfs_utils_from_dvd.sh
@@ -0,0 +1,17 @@
+mount /dev/cdrom /mnt
+
+cat > /etc/yum.repos.d/dvd.repo <<EOF
+[InstallMedia]
+name=Red Hat Enterprise Linux 7.5
+mediaid=1521745267.626363
+metadata_expire=-1
+gpgcheck=1
+cost=500
+enabled=1
+baseurl=file:///mnt/
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+EOF
+
+rpmkeys --import /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+
+yum install -y nfs-utils
