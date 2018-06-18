a5af72bfc67fe99f307db2c80f34962ba6610f9b
Casey Jaymes <cjaymes@gmail.com>
2018-06-18 13:38:08 -0400

    renamed perm_gt,perm_lt

--- test_plugins/tests.py
+++ test_plugins/tests.py
@@ -72,7 +72,7 @@ def odd(value):
     except Exception, e:
         raise errors.AnsibleFilterError('Test error: %s, value=%s' % (str(e),str(value)) )
 
-def less_permissive(value, content):
+def perm_lt(value, content):
     try:
         if isinstance(value, str):
             value = int(value, 8)
@@ -90,7 +90,7 @@ def less_permissive(value, content):
     except Exception, e:
         raise errors.AnsibleFilterError('Test error: %s, value=%s, content=%s' % (str(e),str(value),str(content)) )
 
-def more_permissive(value, content):
+def perm_gt(value, content):
     try:
         if isinstance(value, (str, AnsibleUnsafeText)):
             v = int(value, 8)
@@ -134,8 +134,8 @@ class TestModule(object):
             'lt': lt,
             'lessthan': lt,
 
-            'less_permissive': less_permissive,
-            'more_permissive': more_permissive,
+            'perm_lt': perm_lt,
+            'perm_gt': perm_gt,
 
             'ne': ne,
             'notequalto': ne,
